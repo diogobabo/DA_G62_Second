222a2e7b049add3d9815b1fe7c6dd9371d06fa4c
jogp10 <jogp10@gmail.com>
2022-05-21 19:09:52 +0100

    codigo

--- codigo/main.cpp
+++ codigo/main.cpp
@@ -28,7 +28,7 @@ Graph readFile(const string& file) {
 }
 
 int main() {
-    Graph g = readFile("../input/in01.txt");
+    Graph g = readFile("../input/in01_b.txt");
     Empresa empresa(&g);
     Menu menu(&empresa);
     menu.start();
