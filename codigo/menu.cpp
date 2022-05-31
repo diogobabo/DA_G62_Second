1c82ef6ef1fc4e26cdad3c50191533c5f6d4b385
jogp10 <jogp10@gmail.com>
2022-05-31 17:28:23 +0100

    menu

--- codigo/menu.cpp
+++ codigo/menu.cpp
@@ -18,10 +18,18 @@ int Menu::start() {
 
         switch (option) {
             case 1:
-                Cenario1();
+                if (!Cenario1()) {
+                    cout << endl;
+                    cout << "1) Cenário 1\n";
+                    cout << "2) Cenário 2\n";
+                }
                 break;
             case 2:
-                Cenario2();
+                if (!Cenario2()) {
+                    cout << endl;
+                    cout << "1) Cenário 1\n";
+                    cout << "2) Cenário 2\n";
+                }
                 break;
             default:
                 break;
@@ -39,42 +47,60 @@ int Menu::Cenario1() {
     cout << "Ending point: ";
     cin >> t;
 
-    // Maior capacidade
+    int option = -1;
 
-    int groupD = c1.MaxCapWays(s, t);
+    int groupD;
     vector<int> path;
-    c1.getPath(&path, t);
 
-    cout << "Dimensão do grupo (sem se separar) máxima entre " << s << " para " << t << ": " << groupD << endl;
-    cout << "Número de transbordos: " << path.size()-1 << endl;
-    cout << path[0];
-    for (int i = 1; i < path.size(); i++) {
-        cout << " -> "<< path[i];
+    cout << "1) 1.1\n2) 1.2\n";
+    while (option) {
+        cout << "Insere opção (0 para sair)\n";
+        cin >> option;
+
+        switch (option) {
+            case 1:
+                // Maior capacidade
+                groupD = c1.MaxCapWays(s, t);
+                c1.getPath(&path, t);
+
+                cout << "1.1) Dimensão do grupo (sem se separar) máxima entre " << s << " para " << t << ": " << groupD << endl;
+                cout << "Número de transbordos: " << path.size()-1 << endl;
+                cout << path[0];
+                for (int i = 1; i < path.size(); i++) {
+                    cout << " -> "<< path[i];
+                }
+                cout << endl;
+
+                path.clear();
+                break;
+            case 2:
+                cout << "1.2) " << endl;
+                
+                // Menos transbordos
+                int transbordos = c1.MinTransbordos(s, t);
+                c1.getPath(&path, t);
+                groupD = c1.pathCap(path);
+
+                cout << "Mínimo de transbordos entre " << s << " para " << t << ": " << transbordos << endl;
+                cout << "Dimensão máxima do grupo (sem se separar): " << groupD << endl;
+                cout << path[0];
+                for (int i = 1; i < path.size(); i++) {
+                    cout << " -> "<< path[i];
+                }
+                cout << endl;
+                break;
+            default:
+                break;
+        }
     }
-    cout << endl;
-
-    path.clear();
-    // Menos transbordos
-    int transbordos = c1.MinTransbordos(s, t);
-    c1.getPath(&path, t);
-    groupD = c1.pathCap(path);
-
-    cout << "Mínimo de transbordos entre " << s << " para " << t << ": " << transbordos << endl;
-    cout << "Dimensão máxima do grupo (sem se separar): " << groupD << endl;
-    cout << path[0];
-    for (int i = 1; i < path.size(); i++) {
-        cout << " -> "<< path[i];
-    }
-    cout << endl;
 
     return 0;
-
 }
 
 int Menu::Cenario2() {
     class Cenario2 c2(&e);
 
-    int s, t;
+    int s, t, option = -1;
     cout << "Starting point: ";
     cin >> s;
     cout << "Ending point: ";
@@ -83,25 +109,55 @@ int Menu::Cenario2() {
     Graph residual = this->e.getGraph().createResidual();
     vector<vector<int>> paths;
 
-    int max_flow = c2.fordFulkerson(residual, s, t, &paths);
-    cout << "Ford-Fulkerson max_flow : " << max_flow << endl;
+    int max_flow;
+    int max_time;
+    int dimension, plus;
+    cout << "1) 2.1-2.2\n2) 2.3\n3) 2.4-2.5\n";
+    while (option) {
+        cout << "Insere opção (0 para sair)\n";
+        cin >> option;
 
-    int max_time = 0;
-    for (auto path : paths) {
-        int time = this->e.getGraph().getTime(path);
-        max_time = max(max_time, time);
+        switch (option) {
+            case 1:
+                cout << "2.1) Size of the group : \n";
+                cin >> dimension;
 
-        cout << path[0];
-        for (int i = 1; i < path.size(); i++) {
-            cout << " -> "<< path[i];
+                // after algorithm
 
+                cout << "2.2) Resize the group in x unities : x -> ";
+                cin >> plus;
+
+                // algorithm
+                break;
+            case 2:
+                max_flow = c2.fordFulkerson(residual, s, t, &paths);
+                cout << "2.3) Ford-Fulkerson max_flow : " << max_flow << endl;
+
+                max_time = 0;
+                for (auto path : paths) {
+                    int time = this->e.getGraph().getTime(path);
+                    max_time = max(max_time, time);
+
+                    cout << path[0];
+                    for (int i = 1; i < path.size(); i++) {
+                        cout << " -> "<< path[i];
+
+                    }
+                    cout << endl;
+                }
+                cout << "The group will be reunited after : " << max_time << endl;
+
+                break;
+            case 3:
+                cout << "2.4) The group will be reunited after : " << max_time << endl;
+
+                cout << "2.5) \n";
+                break;
+            default:
+                break;
         }
-        cout << endl;
     }
 
-
-
-    cout << "The group will be reunited after : " << max_time << endl;
     return 0;
 
 }
